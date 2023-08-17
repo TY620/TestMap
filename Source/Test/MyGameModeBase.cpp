19a223e6fe1a1f6362e52344243d552a85133b86
KTY <tkim620@naver.com>
2023-08-17 17:51:31 +0900

    세팅

---
+++ Source/Test/MyGameModeBase.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("안녕하세요"));
+	}
+		//언리얼에서 다국어 지원하기 때문에 문자열은 TEXT
+		UE_LOG(LogTemp, Warning, TEXT("Hello World"))
+	
+}
